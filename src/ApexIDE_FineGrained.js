a9d8caf7fa62911b31bd26377e0644586bd3734a
mino0123 <minoaw@gmail.com>
2012-07-02 08:31:57 +0900

    ソース整理

--- src/ApexIDE_FineGrained.js
+++ src/ApexIDE_FineGrained.js
@@ -1,6 +1,9 @@
-
 var s = document.createElement('script');
-var html = 'apex.ide.IDE.on("configLoaded", function() {apex.ide.IDE.fineGrained = true;setTimeout(function(){apex.ide.IDE.fineGrained=true}, 0)});';
-html += '(function() {var listeners = apex.ide.IDE.events.configloaded.listeners;listeners.unshift(listeners.pop());})();';
-s.innerHTML = html;
-document.head.insertBefore(s, document.head.firstChild);
+s.innerHTML = '(' + function() {
+	apex.ide.IDE.on("configLoaded", function() {
+		apex.ide.IDE.fineGrained = true;
+	});
+	var listeners = apex.ide.IDE.events.configloaded.listeners;
+	listeners.unshift(listeners.pop());
+} + ')();';
+document.head.appendChild(s);
